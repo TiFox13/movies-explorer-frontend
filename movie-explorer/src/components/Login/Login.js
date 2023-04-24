b024d86504e12c9877c67251c7e2b96fb6e17ac6
TiFox <mozi.rin@yandex.ru>
2023-04-24 15:02:12 +0300

    feat: добавить валидацию форм. передалть кнопки редактирования профиля

--- movie-explorer/src/components/Login/Login.js
+++ movie-explorer/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom'; 
 
 import headerLogo from '../../images/headerLogo.svg';
@@ -10,12 +10,39 @@ function Login({}) {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   
+  const [emailError, setEmailError] = React.useState('');
+  const [passwordError, setPasswordError] = React.useState('');
+  const [formValid, setFormValid] = React.useState(false);
+
+  useEffect(() => {
+    if (emailError ) {
+      setFormValid(false)
+    } else {
+      setFormValid(true)
+
+    }
+  }, [emailError, passwordError]);
+
+
   function handleChangeEmail(e) {
     setEmail(e.target.value);
+    const link =/^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/;
+    if (!link.test(String(e.target.value).toLocaleLowerCase())) {
+      setEmailError('Некорректный формат E-mail')
+    } else {
+      setEmailError('')
+    }
   }
   function handleChangePassword(e) {
     setPassword(e.target.value);
+    if (2 <= e.target.value.length)  {
+      console.log(e.target.value.length)
+      setPasswordError('');
+    } else {
+      setPasswordError('Пароль не может быть короче 2 символов')
+    }
   }
+
 
   return (
     <div className='login'>
@@ -26,13 +53,13 @@ function Login({}) {
           <h2 className="form__heading">Рады видеть!</h2>
           <label className = "form__input-field">E-mail
               <input value={email} onChange={handleChangeEmail} type="text" id ="email-input" className="form__item form__item_email" name="email"  required minLength="2" maxLength="200" />
-              <span className = "form__item-error email-input-error"></span>
+              <span className = "form__item-error email-input-error">{emailError}</span>
           </label>
           <label className = "form__input-field">Пароль
               <input value={password} onChange={handleChangePassword} type="text" id ="password-input" className="form__item form__item_password" name="password"  required minLength="4" maxLength="200" />
-              <span className = "form__item-error password-input-error"></span>
+              <span className = "form__item-error password-input-error">{passwordError}</span>
           </label>
-          <input type="submit"  className=" button login-save-button" value='Войти'  aria-label="Войти" />
+          <input type="submit" disabled={!formValid} className=" button login-save-button" value='Войти'  aria-label="Войти" />
         </form>
         <p className='login-paragraph'>Eще не зарегистрированы?  <Link to='/signup' className='link link_login'>Регистрация</Link></p>
       </div>
--- movie-explorer/src/components/Profile/Profile.js
+++ movie-explorer/src/components/Profile/Profile.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useNavigate } from "react-router-dom"
 
 import './Profile.css';
 
 function Profile() {
-  const [name, setName] = React.useState('');
-  const [email, setEmail] = React.useState('');
+  const [name, setName] = React.useState('Виталий');
+  const [email, setEmail] = React.useState('pochta@yandex.ru');
+  const [formActive, setFormActive] = React.useState(false)
+  
 
   function handleChangeName(e) {
     setName(e.target.value);
@@ -12,24 +15,80 @@ function Profile() {
   function handleChangeEmail(e) {
     setEmail(e.target.value);
   }
+
+  function handlePatchClick() {
+    if (formActive === false) {
+     return setFormActive(true)
+    }
+}
+
+const [formValid, setFormValid] = React.useState(false);
+const [nameError, setNameError] = React.useState('');
+const [emailError, setEmailError] = React.useState('');
+
+useEffect(() => {
+  if (nameError || emailError) {
+    setFormValid(false)
+  } else {
+    setFormValid(true)
+  }
+}, [nameError, emailError]);
+
+
+function handleChangeName(e) {
+  setName(e.target.value);
+  if (2 <= e.target.value.length || e.target.value.length >= 30)  {
+    console.log(e.target.value.length)
+    setNameError('');
+  } else {
+    setNameError('Имя должно содержать больше 2 символов и менее 30')
+  }
+
+}
+function handleChangeEmail(e) {
+  setEmail(e.target.value);
+  const link =/^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/;
+  if (!link.test(String(e.target.value).toLocaleLowerCase())) {
+    setEmailError('Некорректный формат E-mail')
+  } else {
+    setEmailError('')
+  }
+}
+
+const [userName, setUserName] = React.useState('Виталий');
+
+function handleSubmit(e) {
+  e.preventDefault();
+  setUserName(name);
+  setFormActive(false);
+}
+
+const navigate = useNavigate();
+  
+  const navigateMovies = (e) => {
+      navigate("/movies");
+  };
+
+
   return (
     <section className='profile'>
       <form className='profile-form' name='profile-form' method='post'>
-        <h2 className=" profile__heading">Привет, Виталий!</h2>
-        <fieldset className='text-inputs-fieldset' disabled>
+        <h2 className=" profile__heading">{`Привет, ${userName}!`}</h2>
+        <fieldset className='text-inputs-fieldset' disabled={!formActive}>
           <label className='profile-form__label'>
             Имя          
-            <input type="text"  className="profile-form__input" value="Виталий" onChange={handleChangeName}/>
+            <input type="text"  className="profile-form__input" value={name} onChange={handleChangeName}/>
           </label>
           <label className='profile-form__label'>
             E-mail
-            <input type="text"  className="profile-form__input" value="pochta@yandex.ru" onChange={handleChangeEmail}/>
+            <input type="text"  className="profile-form__input" value={email} onChange={handleChangeEmail}/>
           </label>
         </fieldset>
-    
-        <input type="button"  className="profile-button" value='Редактировать' aria-label="Редактировать" />
-        <input type="submit"  className="profile-button" value="Сохранить" aria-label="Сохранить" hidden/>
-        <input type="button"  className="profile-button profile-button_sign-out" value="Выйти из аккаунта" aria-label="Выйти из аккаунта" />
+
+        <span className = "form__item-error name-input-error_profile">{nameError ||  emailError}</span>
+        <input type="button" onClick={handlePatchClick} className="button profile-button" value='Редактировать' aria-label="Редактировать" hidden={formActive}/>
+        <input type="submit"  onClick={handleSubmit} className=" button profile-save-button" value="Сохранить" aria-label="Сохранить" hidden={!formActive} disabled={!formValid}/>
+        <input type="button" onClick={navigateMovies} className="button profile-button profile-button_sign-out" value="Выйти из аккаунта" aria-label="Выйти из аккаунта" hidden={formActive}/>
       </form>
     </section>
 
